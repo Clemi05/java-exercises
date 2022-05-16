1bdd77f3ad10a2214c8ac5c7f56af8053f369703
Clement Azalbert <clementazalbert04@gmail.com>
2022-05-16 21:53:01 +0200

    added code drawing a tree

--- 01-Tree/Tree.java
+++ 01-Tree/Tree.java
@@ -2,5 +2,17 @@ public class Tree {
   public static void main(String[] args) {
     // Introduce yourself to users
     System.out.println("Hey there! My name is Clémi and I am learning Java!");
+
+    // Plant a tree
+    System.out.println("Ready to get my hands dirty!");
+
+    System.out.println("  ******  ");
+    System.out.println("  ******  ");
+    System.out.println("  ******  ");
+    System.out.println("  ******  ");
+    System.out.println("  ******  ");
+    System.out.println("    **  ");
+    System.out.println("    **  ");
+    System.out.println("    **  ");
   }
 }
