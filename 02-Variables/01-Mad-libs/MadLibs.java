1d80b6de32a5ecf6bc8148fefed05c6db5dd398b
Clement Azalbert <clementazalbert04@gmail.com>
2022-05-17 22:01:42 +0200

    added all variables

--- 02-Variables/01-Mad-libs/MadLibs.java
+++ 02-Variables/01-Mad-libs/MadLibs.java
@@ -4,9 +4,23 @@ public class MadLibs {
   Author: Clémi
   Date: 17/05/2022
   */
-  	public static void main(String[] args){
+  	public static void main(String[] args) {
+      // Replace the variables with your own values!
+      String name1 = "Clémi";
+      String name2 = "Max";
+      String adjective1 = "fantastic";
+      String adjective2 = "crazy";
+      String adjective3 = "exited";
+      String verb1 = "danse";
+      String noun1 = "actor";
+      String noun2 = "hair";
+      String noun3 = "queen";
+      String noun4 = "pizza";
+      String noun5 = "rain";
+      String noun6 = "ice";
+      String place1 = "London";
 
-
+      int number = 4;
 
       //The template for the story
       String story = "This morning " + name1 + " woke up feeling " + adjective1
@@ -17,5 +31,7 @@ public class MadLibs {
                     + name2 + ", who flew " + name1 + " to " + place1 + " and dropped "
                     + name1 + " in a puddle of frozen " + noun5 + ". " + name1 + " woke up in the year "
                     + number + ", in a world where " + noun6 + "s ruled the world.";
+
+      System.out.println(story);
     }
 }
