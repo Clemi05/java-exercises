3a2b3363586e21b5200ef3252d3f7a7906757c35
Clement Azalbert <clementazalbert04@gmail.com>
2022-05-17 22:58:49 +0200

    updated code with compound assignment operators

--- 02-Variables/02-Math-Magic/Magic.java
+++ 02-Variables/02-Math-Magic/Magic.java
@@ -1,15 +1,24 @@
 public class Magic {
 
 	public static void main(String[] args) {
-    // myNumber is the original number
-    int myNumber = 9187;
-    int stepOne = myNumber * myNumber;
-		int stepTwo = stepOne + myNumber;
-    int stepThree = stepTwo / myNumber;
-    int stepFour = stepThree + 17;
-    int stepFive = stepFour - myNumber;
-    int stepSix = stepFive / 6;
 
-    System.out.println(stepSix);
+    / myNumber is the original number
+    int myNumber = 9;
+    int magicNumber = myNumber * myNumber;
+    // Using compound assignment operators
+    magicNumber += myNumber;
+    magicNumber /= myNumber;
+    magicNumber += 17;
+    magicNumber -= myNumber;
+    magicNumber /= 6;
+
+    // int stepOne = myNumber * myNumber;
+		// int stepTwo = stepOne + myNumber;
+    // int stepThree = stepTwo / myNumber;
+    // int stepFour = stepThree + 17;
+    // int stepFive = stepFour - myNumber;
+    // int stepSix = stepFive / 6;
+
+    System.out.println(magicNumber);
 	}
 }
