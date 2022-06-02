ccbfe93fe3eca64e5ed4ce5fe4e7ea42bf9cffd9
Clement Azalbert <clementazalbert04@gmail.com>
2022-06-02 22:58:16 +0200

    added new elements to arraylist + removing

--- 05-Array-and-ArrayList/01-Desert-Island-Playlist/Playlist.java
+++ 05-Array-and-ArrayList/01-Desert-Island-Playlist/Playlist.java
@@ -10,8 +10,22 @@ class Playlist {
     desertIslandPlaylist.add("Something in the way - Nirvana");
     desertIslandPlaylist.add("Weight of Love - The Black Keys");
     desertIslandPlaylist.add("Notes pour trop tard - Orelsan");
+    desertIslandPlaylist.add("Thunderstruck - AC/DC");
+    desertIslandPlaylist.add("Purple Hearts - Kendrick Lamar");
+    desertIslandPlaylist.add("Champagne Supernova - Oasis");
 
+    // Printing current desertIslandPlaylist and size
     System.out.println(desertIslandPlaylist);
+    System.out.println(desertIslandPlaylist.size());
+
+    // Removing some songs from desertIslandPlaylist
+    desertIslandPlaylist.remove(5);
+    desertIslandPlaylist.remove(5);
+    desertIslandPlaylist.remove(3);
+
+    // Printing current desertIslandPlaylist and size
+    System.out.println(desertIslandPlaylist);
+    System.out.println(desertIslandPlaylist.size());
   }
 
 }
