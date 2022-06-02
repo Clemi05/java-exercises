f88e6085cf75e0f9d6b7a53470022f824b27763e
Clement Azalbert <clementazalbert04@gmail.com>
2022-06-02 22:47:31 +0200

    added elements to arraylist

--- 05-Array-and-ArrayList/01-Desert-Island-Playlist/Playlist.java
+++ 05-Array-and-ArrayList/01-Desert-Island-Playlist/Playlist.java
@@ -4,6 +4,14 @@ class Playlist {
 
   public static void main(String[] args) {
     ArrayList<String> desertIslandPlaylist = new ArrayList<String>();
+
+    desertIslandPlaylist.add("Fireside - Artic Monkeys");
+    desertIslandPlaylist.add("Sing about me i'm dying of thirst - Kendrick Lamar");
+    desertIslandPlaylist.add("Something in the way - Nirvana");
+    desertIslandPlaylist.add("Weight of Love - The Black Keys");
+    desertIslandPlaylist.add("Notes pour trop tard - Orelsan");
+
+    System.out.println(desertIslandPlaylist);
   }
 
 }
