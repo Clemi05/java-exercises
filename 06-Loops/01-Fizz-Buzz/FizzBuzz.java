4c2176c937c1b0c347e02fe551d45c94bb67b18b
Clement Azalbert <clementazalbert04@gmail.com>
2022-06-07 22:56:39 +0200

    added comments

--- 06-Loops/01-Fizz-Buzz/FizzBuzz.java
+++ 06-Loops/01-Fizz-Buzz/FizzBuzz.java
@@ -2,10 +2,13 @@ class FizzBuzz {
 
   public static void main(String[] args) {
     for (int i = 1; i <= 100; i++) {
+      // Check if i is a multiple of 3
       if (i % 3 == 0) {
         System.out.println("Fizz");
+      // Check if i is a multiple of 3
       } else if (i % 5 == 0) {
         System.out.println("Buzz");
+      // Check if i is a multiple of both 3 and 5
       } else if (i % 3 == 0 && i % 5 == 0) {
         System.out.println("FizzBuzz");
       } else {
